ea2bf11de9bf7618dcb827e81d2c1e2876ceeea2
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-09-03 14:37:02 +0300

    d

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Header} from './components';
 import {Home, Cart} from "./pages";
 import {Route} from 'react-router-dom';
 
 function App() {
+    const [pizzas, setPizzas] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/pizzas').then((res) => res.json()).then(data => {
+            setPizzas(data.pizzas);
+        })
+    }, [])
+
   return (
       <div className="wrapper">
           <Header />
           <div className="content">
-              <Route path="/" exact component={Home} />
+              <Route path="/" exact render={() => <Home pizzas={pizzas} /> } />
               <Route path="/cart" exact component={Cart} />
           </div>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 
 export {default as Cart} from './Cart'
 export {default as Home} from './Home'
+export {default as PizzaBlock} from './PizzaBlock'
+
 
 
