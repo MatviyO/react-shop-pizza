d40e6d83d842935b07a343fdfc12ca3caa4288d9
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-10-15 22:24:57 +0300

    dfd

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, {useEffect} from 'react';
 import {Route} from 'react-router-dom';
-import axios from 'axios';
 import { useDispatch} from "react-redux";
-
 import {Header} from './components';
 import {Home, Cart} from "./pages";
-import {setPizzas } from "./redux/actions/pizzas";
+import {fetchPizzas } from "./redux/actions/pizzas";
 
 function App() {
     const dispatch = useDispatch();
     
     useEffect(() => {
-        axios.get('http://localhost:3001/pizzas')
-            .then(({data}) => dispatch(setPizzas(data)));
+        fetchPizzas();
     }, []);
-
 
     return (
         <div className="wrapper">
